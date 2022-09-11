64a4d22ac2c6a8248fb9fe6e4888f909d89bf932
Glenn chan <glenncyop@gmail.com>
2022-09-10 21:10:19 -0700

    Project first commit

---
+++ maven.eclipse_playground/src/main/java/com/sddevops/maven/eclipse_playground/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven.eclipse_playground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
